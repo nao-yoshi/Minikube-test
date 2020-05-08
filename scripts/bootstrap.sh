f55b00709d6b50bf0f8401c3e4e32b0d59f1af63
nao-yoshi <61136455+nao-yoshi@users.noreply.github.com>
2020-05-08 12:33:21 +0900

    v0.6

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -19,9 +19,12 @@ echo "========================================="
 sudo systemctl stop firewalld
 sudo systemctl disable firewalld
 
-## Install git
+## Install git Node.js
 echo "========================================="
 echo "Start git Installation"
 echo "========================================="
-sudo yum install -y git
+sudo yum install -y git 
+curl -sL https://rpm.nodesource.com/setup_10.x | bash -
+sudo yum install -y epel-release
+sudo yum install -y nodejs
 echo "========================================="
